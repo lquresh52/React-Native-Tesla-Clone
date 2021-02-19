92362da7ca4c4523223daafd434ec771b62007b4
Quresh Lakdawala <lquresh52@gmail.com>
2021-02-19 12:20:06 +0530

    Inital commit

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ImageBackground, View,  } from 'react-native';
+import CarsList from './components/CarsList';
+import Headers from './components/Headers'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+
+		<Headers />			
+
+		<CarsList />
+
+
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
---
+++ components/CarItem/styles.js
@@ -0,0 +1,41 @@
+import {StyleSheet, Dimensions} from 'react-native';
+
+
+const styles = StyleSheet.create({
+
+    carContainer: {
+		width: Dimensions.get('window').width,
+		height: Dimensions.get('window').height,
+	},
+	titles: {
+		width: '100%',
+		marginTop: '30%',
+		alignItems: 'center',
+	},
+	title: {
+		fontSize: 40,
+		fontWeight: '500',
+	},
+	subtitle: {
+		fontSize: 16,
+		color: '#5c5e62',
+	},
+	subtitleCTA: {
+		textDecorationLine: 'underline',
+	},
+	image: {
+		width: '100%',
+		height: '100%',
+		resizeMode: 'cover',
+		position: 'absolute',
+	},
+    buttonsContainer: {
+        position: 'absolute',
+        bottom: 50,
+        width: '100%',
+    },
+
+
+});
+
+export default styles;
